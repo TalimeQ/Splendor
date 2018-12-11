79ec7f825dd82433ab38fc89d2cdc93bb6d0071b
TalimeQ <boren1996@wp.pl>
2018-12-11 17:45:14 +0100

    #Mobile Support Start

--- Source/InzynierkaSplendor/Public/Player/PlayerPawn.h
+++ Source/InzynierkaSplendor/Public/Player/PlayerPawn.h
@@ -28,6 +28,7 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+	UFUNCTION(BlueprintCallable)
 	void InitializeRaycast(FVector mousePos, FVector mouseDir);
 
 private:
